149c118302248d6c97defca5882deaf0ba119297
Anh Nam Tran <atran35@attu6.cs.washington.edu>
2018-12-26 10:44:44 -0800

    Fixed typo

--- test_cache.sh
+++ test_cache.sh
@@ -21,19 +21,20 @@ function run_exp {
   redis-cli FLUSHALL
 
   pushd examples > /dev/null
-  cozy $1.ds -t $TIME_OUT --no-cost-model-cache | tee "${NO_CACHE}/$1.log"
+  cozy $1.ds -t $TIME_OUT --no-cost-model-cache > "${NO_CACHE}/$1.log"
   popd
 
   COUNTER=0
   while [ $COUNTER -lt $WARM_UP_LIMIT ]; do
+    echo $COUNTER
 
     pushd examples > /dev/null
-    cozy $1.ds -t $TIME_OUT | tee "${CACHE}/$1.log"
+    cozy $1.ds -t $TIME_OUT > "${CACHE}/$1.log"
     popd
 
     ./vis.py $1 $COUNTER
 
-    let COUNER=COUNTER+1
+    let COUNTER=COUNTER+1
   done
 
   update_summary $1
@@ -53,9 +54,9 @@ mkdir -p $NO_CACHE
 > $SUMMARY
 
 run_exp maxbag
-run_exp tpchq5
-run_exp clausedb
-run_exp nested-map
-run_exp disjunction
-run_exp graph
-run_exp map
+#run_exp tpchq5
+#run_exp clausedb
+#run_exp nested-map
+#run_exp disjunction
+#run_exp graph
+#run_exp map
