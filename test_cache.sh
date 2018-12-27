3174d3837297c8ef7e56bdcac740d8110fa3e140
Anh Nam Tran <atran35@attu6.cs.washington.edu>
2018-12-27 13:46:33 -0800

    Some changes to how data is collected

--- test_cache.sh
+++ test_cache.sh
@@ -10,7 +10,7 @@ EXP_OUT="${PWD}/exp_out"
 CACHE="${EXP_OUT}/cache"
 NO_CACHE="${EXP_OUT}/no_cache"
 
-SUMMARY="${EXP_OUT}/summary.log"
+SUMMARY="${EXP_OUT}/summary.txt"
 
 WARM_UP_LIMIT=3
 if [ $# -eq 2 ]; then
@@ -25,37 +25,42 @@ fi
 function run_exp {
   echo "Flushing Cache"
   redis-cli FLUSHALL
+  COUNTER=0
 
   echo "------------------------------------------"
   echo "------------------------------------------"
   echo "Running baseline on $1"
+  echo "Run no. $COUNTER"
   pushd examples > /dev/null
-  cozy $1.ds -t $TIME_OUT --no-cost-model-cache > "${NO_CACHE}/$1.log"
+  cozy $1.ds -t $TIME_OUT --no-cost-model-cache > "${NO_CACHE}/$1.txt"
   popd
+
+  ./vis.py $1 $COUNTER
+  let COUNTER=COUNTER+1
+
+  echo "$1.ds:" >> $SUMMARY
+  tail -n 1 < "${NO_CACHE}/$1.txt" >> $SUMMARY
 
   echo "------------------------------------------"
   echo "Cache warmup limit: $WARM_UP_LIMIT"
-  COUNTER=0
-  while [ $COUNTER -lt $WARM_UP_LIMIT ]; do
+  while [ $COUNTER -le $WARM_UP_LIMIT ]; do
     echo "------------------------------------------"
     echo "Run no. $COUNTER"
 
     pushd examples > /dev/null
-    cozy $1.ds -t $TIME_OUT > "${CACHE}/$1.log"
+    cozy $1.ds -t $TIME_OUT > "${CACHE}/$1-$COUNTER.txt"
     popd
+
+    update_summary $1 $COUNTER
 
     ./vis.py $1 $COUNTER
 
     let COUNTER=COUNTER+1
   done
-
-  update_summary $1
 }
 
 function update_summary {
-  echo "$1.ds:" >> $SUMMARY
-  tail -n 1 < "${NO_CACHE}/$1.log" >> $SUMMARY
-  tail -n 1 < "${CACHE}/$1.log" >> $SUMMARY
+  tail -n 1 < "${CACHE}/$1-$2.txt" >> $SUMMARY
 
   echo
 }
@@ -67,7 +72,7 @@ mkdir -p $NO_CACHE
 
 run_exp maxbag
 run_exp tpchq5
-run_exp clausedb
+#run_exp clausedb
 run_exp nested-map
 run_exp disjunction
 run_exp graph
