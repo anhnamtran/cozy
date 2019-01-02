033456a4bd1f642a432619282155e8ba851cb0dc
Anh Nam Tran <atran35@miles.cs.washington.edu>
2019-01-02 11:57:53 -0800

    Added line breaks

--- hit_rates.py
+++ hit_rates.py
@@ -25,6 +25,6 @@ if not os.path.isdir(file_out_dir):
     os.makedirs(file_out_dir)
 
 with open("{}/run-{}.txt".format(file_out_dir, run_no), "w+") as f:
-    f.write("Hits: {}".format(hits))
-    f.write("Misses: {}".format(misses))
+    f.write("Hits: {}\n".format(hits))
+    f.write("Misses: {}\n".format(misses))
     f.write("Hit rate: {}".format(hit_rate))
