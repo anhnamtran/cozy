5a3d5fa9c3683b74840db5308aed424f50eb942b
Andrew Tran <andremail03@gmail.com>
2019-01-22 18:45:32 -0800

    Added simple bash script

---
+++ test_cache_simple.sh
@@ -0,0 +1,31 @@
+export PYTHONUNBUFFERED=1
+
+OUT_DIR="/tmp/cozy-logs"
+
+# $1: exp name
+# $2: out name
+function run_exp {
+  pushd cache_examples
+
+  { time cozy --no-cost-model-cache $1.ds --java $2.java &> $OUT_DIR/$1.log ; } &> $1-no-cache.time
+
+  run_driver $1 $2
+
+  { time cozy $1 --java $1.java &> $OUT_DIR/$1.log ; } &> $1-cache.time
+
+  run_driver $1 $2
+
+  popd
+}
+
+# $1: exp name
+# $2: driver name
+function run_driver {
+  javac -cp .:* *.java
+
+  java $2Main
+}
+
+mkdir $OUT_DIR
+
+run_exp maxbag MaxBag
