8d47d1c1f0c20c25c214615a37655eacd98e584d
Anh Nam Tran <atran35@thelonious.cs.washington.edu>
2019-01-22 19:11:21 -0800

    Refined some bugs

--- test_cache_simple.sh
+++ test_cache_simple.sh
@@ -1,19 +1,22 @@
 export PYTHONUNBUFFERED=1
 
-set -x
+set -e
 
 OUT_DIR="/tmp/cozy-logs"
 
 # $1: exp name
 # $2: out name
 function run_exp {
+  redis-cli FLUSHALL
   pushd cache_examples
 
-  { time cozy --no-cost-model-cache $1.ds --java $2.java &> $OUT_DIR/$1.log ; } &> $1-no-cache.time
+  echo "Running Cozy with no cache"
+  { time cozy --no-cost-model-cache $1.ds --java $2.java &> $OUT_DIR/$1-no-cache.log ; } &> $1-no-cache.time
 
   run_driver $1 $2
 
-  { time cozy $1.ds --java $1.java &> $OUT_DIR/$1.log ; } &> $1-cache.time
+  echo "Running Cozy with cache"
+  { time cozy $1.ds --java $2.java &> $OUT_DIR/$1-cache.log ; } &> $1-cache.time
 
   run_driver $1 $2
 
@@ -23,11 +26,12 @@ function run_exp {
 # $1: exp name
 # $2: driver name
 function run_driver {
+  rm -rf *.class
   javac -cp .:* *.java
 
   java $2Main
 }
 
-mkdir $OUT_DIR
+mkdir -p $OUT_DIR
 
 run_exp maxbag MaxBag
